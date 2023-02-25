8838ed29b4f3f320d426d5a6891973abcffc5af1
naoki111 <2004240028yn@edu.oca.ac.jp>
2023-02-25 13:54:06 +0900

    commit

---
+++ ex5.js
@@ -0,0 +1,8 @@
+a = ["a"]
+b = ["b"]
+c = ["c"]
+a.push("aa")
+a.pop()
+a
+abc = [...a,...b,...c]
+abc
---
+++ ex6.js
@@ -0,0 +1,19 @@
+a = {"id":1,"name":"a"}
+b = {"id":2,"name":"b"}
+ab = {...a,...b}
+ab
+a.push("aa":"aa")
+
+
+
+ab
+a
+b
+
+
+
+
+a
+b
+
+ab
