873bf11a7a5ec33cbf198092463534a742a91ba9
naoki111 <2004240028yn@edu.oca.ac.jp>
2023-02-25 14:36:09 +0900

    commit

---
+++ .ex8.js
@@ -0,0 +1,3 @@
+for(let a = 0; a < 3; a++){
+if(a = 2){console.log(a)
+;}}
---
+++ ex9.js
@@ -0,0 +1,20 @@
+function num(a,b){
+sum = a + b;
+console.log(sum);
+return sum;
+}
+num(1,2)
+
+const num1 = function(a,b){
+sum = a + b;
+console.log(sum);
+return sum;
+}
+num1(1,2)
+
+const num2 = (a,b) =>{
+sum = a + b;
+console.log(sum);
+return sum;
+}
+num2(1,2)
